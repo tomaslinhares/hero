82b095aeb432d1ceb1a5d92ea113e6c7f9a10a60
tomaslinhares <tomasflinhares@gmail.com>
2023-10-07 17:32:13 +0100

    Fifth Step

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,15 +1,30 @@
-import com.googlecode.lanterna.Terminal;
+import com.googlecode.lanterna.TerminalSize;
+import com.googlecode.lanterna.TextCharacter;
 import com.googlecode.lanterna.screen.Screen;
+import com.googlecode.lanterna.screen.TerminalScreen;
 import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
-import com.googlecode.lanterna.terminal.TerminalScreen;
+import com.googlecode.lanterna.terminal.Terminal;
 
+import java.io.IOException;
 public class Application {
-    public static void main(String[] args) {
-        Terminal terminal = new
-        DefaultTerminalFactory().createTerminal();
+    public static void main(String[] args) throws IOException {
+        try {
+            Terminal terminal = new
+                    DefaultTerminalFactory().createTerminal();
+            Screen screen = new TerminalScreen(terminal);
+            screen.setCursorPosition(null); // we don't need a cursor
+            screen.startScreen(); // screens must be started
+            screen.doResizeIfNecessary(); // resize screen if necessary
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        TerminalSize terminalSize = new TerminalSize(40, 20);
+        DefaultTerminalFactory terminalFactory = new
+        DefaultTerminalFactory().setInitialTerminalSize(terminalSize);
+        Terminal terminal = terminalFactory.createTerminal();
         Screen screen = new TerminalScreen(terminal);
-        screen.setCursorPosition(null); // we don't need a cursor
-        screen.startScreen(); // screens must be started
-        screen.doResizeIfNecessary(); // resize screen if necessary
+        screen.clear();
+        screen.setCharacter(10, 10, TextCharacter.fromCharacter('X') [0]);
+        screen.refresh();
     }
 }
